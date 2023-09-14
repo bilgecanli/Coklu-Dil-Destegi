067f1c1b872dccc7d23c633361929294e8e35af5
mehmet <mehmetsaidcanli@gmail.com>
2023-09-14 10:19:11 +0300

    Initial Commit

---
+++ Coklu Dil Destegi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coklu Dil Destegi
+//
+//  Created by Mehmet Said Canlı on 14.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
